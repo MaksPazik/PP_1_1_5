bfb9e20173d5f0a3671f0cbe06592705f1fa8b2e
Max Paz <makshookah@mail.ru>
2022-12-07 23:24:47 +0300

    Правка

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -9,22 +9,22 @@ import static jm.task.core.jdbc.util.Util.getConnection;
 
 public class Main {
     static UserServiceImpl userService = new UserServiceImpl();
+
     public static void main(String[] args) {
+
         Util util = new Util();
         util.getConnection();
+
         userService.createUsersTable();
-        userService.cleanUsersTable();
+
         userService.saveUser("Max", "Pasevich", (byte) 21);
         userService.saveUser("Svetlana", "Grizheliyk", (byte) 18);
         userService.saveUser("Ivan", "Ivanov", (byte) 99);
         userService.saveUser("Oleg", "OLegov", (byte) 99);
+
         userService.getAllUsers();
-
-//        userService.cleanUsersTable();
-//        userService.dropUsersTable();
-
-
-
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
 
 
         util.closeConnection();
